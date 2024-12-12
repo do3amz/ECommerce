ae64ce2ba8393537a5b661a898787a8b5e1ef96e
doaa zhran <do3amz@hotmail.com>
2024-12-12 09:13:44 +0200

    pagination

--- ECommerce/Services/Catalog/Catalog.API/Products/ListProducts/GetProductsEndPoint.cs
+++ ECommerce/Services/Catalog/Catalog.API/Products/ListProducts/GetProductsEndPoint.cs
@@ -4,6 +4,7 @@ using System.Threading;
 namespace Catalog.API.Products.ListProducts
 {
 	//request
+	public record GetProductsRequest(int? PageNumber=1,int? PageSize=10);
 	//response
 	public record GetProductsResponse(IEnumerable<Product> Products);
 	public class GetProductsEndPoint : ICarterModule
@@ -11,9 +12,10 @@ namespace Catalog.API.Products.ListProducts
 	{
 		public void AddRoutes(IEndpointRouteBuilder app)
 		{
-			app.MapGet("/products", async (ISender sender) => 
+			app.MapGet("/products", async ([AsParameters] GetProductsRequest request,ISender sender) => 
 			{
-				var result = await sender.Send(new GetProductsQuery());
+				var query = request.Adapt<GetProductsQuery>();
+				var result = await sender.Send(query);
 			     var response=result.Adapt<GetProductsResponse>();
 				return Results.Ok(response);
 			}).WithName("GetProducts").Produces<GetProductsResponse>(StatusCodes.Status200OK)
