4a57342a968fb64bb113b618275ee674858ef9ce
doaa zhran <do3amz@hotmail.com>
2024-12-20 12:56:15 +0200

    CreateDiscount service

---
+++ ECommerce/Services/Discount/Discount.grpc/Data/DiscountContext.cs
@@ -0,0 +1,13 @@
+﻿using Discount.grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.grpc.Data
+{
+	public class DiscountContext : DbContext
+	{
+		public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+		{
+		}
+		public DbSet<Coupon> Coupons { get; set; } = default!;
+	}
+}
---
+++ ECommerce/Services/Discount/Discount.grpc/Services/DiscountService.cs
@@ -0,0 +1,52 @@
+﻿using Discount.grpc.Data;
+using Discount.grpc.Models;
+using Discount.grpc.Protos;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.grpc.Services
+{
+	public class DiscountService(DiscountContext discountContext,ILogger<DiscountService> logger):DiscountProtoService.DiscountProtoServiceBase
+	{
+		public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+		{
+			var coupon= await discountContext.Coupons.FirstOrDefaultAsync(o=>o.ProductName==request.ProductName);
+			if (coupon is null)
+			{
+				coupon = new Coupon
+				{
+					ProductName = "No Discount",
+					Amount = 0,
+					Description = "No description"
+
+				};
+			}
+				logger.LogInformation("Discount is retreived for product name :{productName},Amount:{amount}",coupon.ProductName,coupon.Amount);
+				var couponModel = coupon.Adapt<CouponModel>();
+				return couponModel;
+			
+		}
+		public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+		{
+			var coupon = request.Coupon.Adapt<Coupon>();
+			if (coupon is null)
+			{
+				throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request object."));
+			}
+			discountContext.Coupons.Add(coupon);
+			await discountContext.SaveChangesAsync();
+			logger.LogInformation("Discount is successfully created.ProductName :{ProductName}",coupon.ProductName);
+			var couponModel = coupon.Adapt<CouponModel>();
+			return couponModel;
+		}
+		public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+		{
+			return base.UpdateDiscount(request, context);
+		}
+		public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+		{
+			return base.DeleteDiscount(request, context);
+		}
+	}
+}
